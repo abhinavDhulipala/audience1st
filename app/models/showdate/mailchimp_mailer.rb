57fd456506b8df02280574cd8f90598f326883cc
abhinavDhulipala <abhinav.dhulipala@berkeley.edu>
2021-05-04 13:05:24 -0700

    implement Armondo's changes

--- app/models/showdate/mailchimp_mailer.rb
+++ app/models/showdate/mailchimp_mailer.rb
@@ -1,6 +1,9 @@
 require 'digest'
 
 class MailchimpMailer
+
+  attr_reader @errors
+
   def initialize(key = nil)
     @apikey = key || (Option.mailchimp_key rescue nil)
     @disabled = !!@apikey.blank?
@@ -8,38 +11,30 @@ class MailchimpMailer
   end
 
   def mailchimp_init(key)
-    begin
+    mailchimp_handler do
       @mailchimp = MailchimpMarketing::Client.new
       @mailchimp.set_config({
-                              :api_key => key,
-                              :server => "us1"
+                              api_key: key,
+                              server: "us1"
                             })
-      response = @mailchimp.ping.get
-      response
-    rescue MailchimpMarketing::ApiError => e
-      @errors << e
-      p Rails.logger.info "mailchimp init: #{@errors}"
-    end
+      @mailchimp.ping.get
+      end
   end 
 
-  #
+  # @returns the first list for a given user or nil if an error was encountered
   def get_list_id(email)
-    begin
+    mailchimp_handler do
       # ref: https://github.com/mailchimp/mailchimp-marketing-ruby/blob/master/lib/MailchimpMarketing/api/lists_api.rb#L181
       response = @mailchimp.lists.get_all_lists({
                    email: email
                  })
-      return 'response has no key', true unless response.key? "lists"
-      lists = response["lists"]
-      return response["lists"][0]["id"], false unless lists.empty?
-      e = 'user not in list'
-      @errors << e
-      return e, true
-    rescue MailchimpMarketing::ApiError => e
-      @errors << e
-      return e, true
+      if response.key? "lists"
+        lists = response["lists"]
+        response["lists"][0]["id"] unless lists.empty?
+      end
     end
   end
+
 
   # batch add/remove list members to static segment
   # def batch_add()
@@ -53,23 +48,19 @@ class MailchimpMailer
   # end
 
   def create_segment(list_id, segment_name, emails_to_add)
-    begin
+    mailchimp_handler do
       # 'name' and 'static segment' are required body params
       # which is an array of emails to be used for a static segment
       # an empty array will create a static segment without any members
-      response = @mailchimp.lists.create_segment(list_id, { 'name' => segment_name, 'static_segment' => emails_to_add })
-      return response
-    rescue MailchimpMarketing::ApiError => e
-      @errors << e
-      return e.to_s, true
-    end
+      @mailchimp.lists.create_segment(list_id, { 'name' => segment_name, 'static_segment' => emails_to_add })
+      end
   end
 
   # https://github.com/mailchimp/mailchimp-marketing-ruby/blob/master/lib/MailchimpMarketing/api/lists_api.rb#L1088
   # For now this method just gets the first segment in the list. Here as a template for future devs
   # @returns str(segment_id or error), bool(whether error was encountered)
   def find_segment(list_id)
-    begin
+    mailchimp_handler do
       # further body params found here:  https://mailchimp.com/developer/marketing/api/list-segments/list-segments/
       response = @mailchimp.lists.list_segments(list_id, {
         exclude_fields: ['_links'],
@@ -77,15 +68,25 @@ class MailchimpMailer
       })
 
       segments = response['segments']
-      return 'no segments available', true if segments.nil? or segments.empty?
-      return segments[0]['id'], false
-    rescue MailchimpMarketing::ApiError => e
-      @errors << e
-      return e.to_s, true
+      unless segments.nil? or segments.empty?
+        segments[0]['id']
+      end
     end
+
   end
 
   private
+
+  def mailchimp_handler &block
+    begin
+      block.call
+    rescue MailchimpMarketing::ApiError => e
+      @errors << e
+      Rails.logger.info "mailchimp init: #{@errors}"
+      nil
+    end
+  end
+
 
   def mailchimp_body_for(customer)
     {
